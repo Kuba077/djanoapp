fb6c9eead0f69ae9321a0b773aaa7b6d116c398c
Kuba077 <mazurjakub81@gmail.com>
2019-10-03 14:17:40 +0200

    Dodanie pizzy i skladnikow

--- pizza/models.py
+++ pizza/models.py
@@ -13,20 +13,28 @@ class Pizza(models.Model):
                              max_length=30,
                              help_text='Nazwa pizzy',)
     opis = models.TextField(blank=True,
+                            default='',
                             help_text='Opis pizzy')
     rozmiar = models.CharField(max_length=1,
                                choices=ROZMIARY,
                                default=LARGE)
     cena = models.DecimalField(max_digits=5,
-                               decimal_places=2)
+                               decimal_places=2,
+                               default=0)
     data = models.DateField('dodano', auto_now_add=True)
 
+    def __str__(self):
+        return self.nazwa
+
 class Skladnik(models.Model):
-    pizza = models.ManyToManyField( Pizza,
+    pizze = models.ManyToManyField( Pizza,
                                     related_name='skladniki')
 
     nazwa = models.CharField('skladnik', max_length=30)
     jarski = models.BooleanField('jarski?',
                                  help_text='Zaznacz, jeżeli składnik jest odpowiedni dla wegetarianina',
                                  default=False)
-    cena = models.DecimalField(max_digits='3', decimal_places=2)
+    cena = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.nazwa
